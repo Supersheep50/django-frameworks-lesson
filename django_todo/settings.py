c76c2da3fe340ba76650dbb05f7069583ce35e16
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-03-01 15:48:05 +0000

    change host name

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@4rtn-t_t6d900vgp&dz=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://jonm1988-django-todo-app.herokuapp.com/']
+ALLOWED_HOSTS = ['jonm1988-django-todo-app.herokuapp.com']
 
 
 # Application definition
